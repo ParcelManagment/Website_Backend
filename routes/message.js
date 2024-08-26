fb00acf6fa4fe8526a44b423e42ba1dc1854890d
Kavindu Wickramasekara <kavi2020wick@gmail.com>
2024-08-26 20:39:20 +0530

    add : getting message from Server to ML server

--- routes/message.js
+++ routes/message.js
@@ -29,21 +29,71 @@ router.get("/default", async (req, res, next) => {
 });
 router.post("/custom", async (req, res, next) => {
   try {
-    const { customMessage,uId,deviceId,dateTime } = req.body;
+    const { customMessage, uId, deviceId, dateTime } = req.body;
 
-    if (!customMessage) {
-      return res.status(400).json({ Error: "No custom message provided" });
-    }
-    res.status(200).json({
+    //for debugging purposes
+    console.log("Received custom message request:", {
       customMessage,
       uId,
       deviceId,
       dateTime,
     });
+
+    // Check fields
+    if (!customMessage) {
+      return res.status(400).json({ Error: "No custom message provided" });
+    }
+    if (!uId) {
+      return res.status(400).json({ Error: "No user ID provided" });
+    }
+    if (!deviceId) {
+      return res.status(400).json({ Error: "No device ID provided" });
+    }
+    if (!dateTime) {
+      return res.status(400).json({ Error: "No date/time provided" });
+    }
+
+
+
+
+     const response = await fetch("http://server-url/custom", {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json",
+       },
+       //custom message chage in the ML server.get from ML cutomMessage
+       body: JSON.stringify({
+         customMessage
+       }),
+     });
+    
+    const data = await response.json();
+
+if (!response.ok) {
+  res.status(200).json({
+    customMessage:"Error",
+    uId,
+    deviceId,
+    dateTime,
+  });
+  console.error("Error:", data.Error);
+} else {
+  res.status(200).json({
+    data,
+    uId,
+    deviceId,
+    dateTime,
+  });
+  console.log("Message sent successfully:", data);
+}
+
+
+    
   } catch (error) {
     console.error("Error sending custom message", error);
     res.status(500).json({ Error: "Something went wrong" });
   }
 });
+
 
 module.exports = router;
