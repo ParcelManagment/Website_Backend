070c88c555031157a83ad301953666fec68ddcf5
Dilan Madhusankha <dilanmadusanka5555@gmail.com>
2024-09-21 05:08:18 +0530

    add summary sub routes

--- routes/summary.js
+++ routes/summary.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 
 router.get("/quicks",async (req, res, next)=>{
-
+    
+    
     const connection = await getConnection();
     if(!connection){
         console.log("Database connection unavailable")
@@ -13,9 +14,26 @@ router.get("/quicks",async (req, res, next)=>{
     }
 
     const query1 = "SELECT COUNT(id) AS users FROM user WHERE role = 'user';"
-    const query2 = `SELECT COALESCE(SUM(price), 0) AS total_Revenue FROM package WHERE submitted >= CURDATE() - INTERVAL 7 DAY;`
-    const query3 = "SELECT COUNT(price) AS orders FROM package WHERE submitted >= CURDATE() - INTERVAL 7 DAY;"
-    const query4 = `SELECT FLOOR(AVG(price)) AS average FROM package WHERE submitted >= CURDATE() - INTERVAL 7 DAY`
+    const query2 = `SELECT 
+                        COALESCE(SUM(price), 0) AS total_Revenue 
+                    FROM 
+                        package 
+                    WHERE 
+                        submitted >= CURDATE() - INTERVAL 7 DAY;`
+
+    const query3 = `SELECT 
+                        COUNT(price) AS orders 
+                    FROM 
+                        package 
+                    WHERE 
+                        submitted >= CURDATE() - INTERVAL 7 DAY;`
+
+    const query4 = `SELECT 
+                        FLOOR(AVG(price)) AS average 
+                    FROM 
+                        package 
+                    WHERE 
+                        submitted >= CURDATE() - INTERVAL 7 DAY`
     try{
         const [[users]] = await connection.query(query1);
         const [[revenue]] = await connection.query(query2);
@@ -28,6 +46,8 @@ router.get("/quicks",async (req, res, next)=>{
     }catch(err){
         res.status(500).json({Error:"Error Getting Data"})
         console.log(err)
+    }finally{
+        connection.release()
     }
 
 })
@@ -42,28 +62,29 @@ router.get('/revenue', async (req, res, next)=>{
     }
 
     const query1 = `WITH RECURSIVE last_seven_days AS (
-    SELECT CURDATE() - INTERVAL 6 DAY AS submission_date
-    UNION ALL
-    SELECT submission_date + INTERVAL 1 DAY
-    FROM last_seven_days
-    WHERE submission_date + INTERVAL 1 DAY <= CURDATE()
-    )
-    SELECT 
-        DATE_FORMAT(d.submission_date, '%d') AS date,
-        DATE_FORMAT(d.submission_date, '%a') AS day,
-        COALESCE(SUM(p.price), 0) AS revenue
-    FROM 
-        last_seven_days d
-    LEFT JOIN 
-        package p ON DATE(p.submitted) = d.submission_date
-    GROUP BY d.submission_date
-    ORDER BY d.submission_date;`
+                    SELECT CURDATE() - INTERVAL 6 DAY AS submission_date
+                    UNION ALL
+                    SELECT submission_date + INTERVAL 1 DAY
+                    FROM last_seven_days
+                    WHERE submission_date + INTERVAL 1 DAY <= CURDATE()
+                    )
+                    SELECT 
+                        DATE_FORMAT(d.submission_date, '%a') AS day,
+                        COALESCE(SUM(p.price), 0) AS revenue
+                    FROM 
+                        last_seven_days d
+                    LEFT JOIN 
+                        package p ON DATE(p.submitted) = d.submission_date
+                    GROUP BY d.submission_date
+                    ORDER BY d.submission_date;`
 
     try{
         const [users] = await connection.query(query1)
         res.status(200).json(users)
     }catch(err){
         res.status(500).json({Error:"Error Getting Data"})
+    }finally{
+        connection.release()
     }
 
 
@@ -78,7 +99,7 @@ router.get('/stations', async (req, res, next)=>{
         return;
     }
 
-    const query = `SELECT departure, COALESCE(SUM(price),0) 
+    const query = `SELECT departure AS station, COALESCE(SUM(price),0) 
                     AS
                         price
                     FROM
@@ -89,7 +110,7 @@ router.get('/stations', async (req, res, next)=>{
                         departure
                     ORDER BY 
                         price DESC
-                    LIMIT 5 `
+                    LIMIT 5`
     
     try{
         const [stations] = await connection.query(query)
@@ -97,8 +118,78 @@ router.get('/stations', async (req, res, next)=>{
     }catch(err){
         console.log(err)
         res.status(500).json({Error:"Error Getting Data"})
+    }finally{
+        connection.release()
     }
                 
+})
+
+router.get('/recentactivity', async (req, res, next)=>{
+    const connection = await getConnection();
+    if(!connection){
+        console.log("Database connection unavailable")
+        res.status(500).json({Error: "Database Error"})
+        return;
+    }
+
+    const query = `(SELECT 
+                        'staff' AS type
+                        ,DATE_FORMAT(created_at, '%Y %b %d') AS date
+                        ,TIME_FORMAT(created_at, '%H:%i') AS time
+                        ,created_by AS ref 
+                    FROM 
+                        station_staff ) 
+                    UNION ALL 
+                    (SELECT 
+                        'device'
+                        ,DATE_FORMAT(installation, '%Y %b %d') AS date
+                        ,TIME_FORMAT(installation, '%H:%i') AS time
+                        ,installed_by 
+                    FROM 
+                        trackingDevice) 
+                    ORDER BY 
+                        date DESC, time DESC 
+                    LIMIT 5`
+
+    try{
+        const [recent] = await connection.query(query)
+        res.status(200).json(recent)
+    }catch(err){
+        console.log(err)
+        res.status(500).json({Error:"Error Getting Data"})
+    }finally{
+        connection.release()
+    }
+})
+
+router.get('/usertypes', async (req ,res, next)=>{
+    const connection = await getConnection();
+    if(!connection){
+        console.log("Database connection unavailable")
+        res.status(500).json({Error: "Database Error"})
+        return;
+    }
+
+    const query = `SELECT 
+                        COUNT(*) as value,
+                        CASE 
+                            WHEN role = 'user_nr' THEN 'Only Email'
+                            WHEN role = 'user' THEN 'App Users'                           
+                        END AS type
+                    FROM 
+                        user
+                    GROUP BY
+                        role`
+
+    try{
+        const [user] = await connection.query(query)
+        res.status(200).json(user)
+    }catch(err){
+        console.log(err)
+        res.status(500).json({Error:"Error Getting Data"})
+    }finally{
+        connection.release()
+    }
 })
 
 module.exports = router
